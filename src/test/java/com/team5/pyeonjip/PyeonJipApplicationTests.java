e861b1ef1e71e31718886ca13fdf733c81d542da
jsjhyun <gusrnl98@naver.com>
2025-02-18 22:45:11 +0900

    test : 재고 감소 동시성 테스트 추가

--- src/test/java/com/team5/pyeonjip/PyeonJipApplicationTests.java
+++ src/test/java/com/team5/pyeonjip/PyeonJipApplicationTests.java
@@ -2,12 +2,13 @@ package com.team5.pyeonjip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PyeonJipApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
